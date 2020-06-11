54e12594525ccd94001b033d44eb0e76cc658332
Rafael Acosta <rafael.r.acosta.m.93@gmail.com>
2020-06-11 18:14:51 -0400

    Desplegando v0.0.1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const PORT_SERVER = process.env.PORT || 3977;
-const {API_VERSION, IP_SERVER, DB_PORT, DB_NAME} = require("./config");
+const {API_VERSION, IP_SERVER, DB_PORT, DB_NAME, DB_USER_PASS} = require("./config");
 
 mongoose.set("useFindAndModify", false);
 
-mongoose.connect(`mongodb://${IP_SERVER}:${DB_PORT}/${DB_NAME}`,
+//mongoose.connect(`mongodb://${IP_SERVER}:${DB_PORT}/${DB_NAME}`,
+mongoose.connect(`mongodb+srv://rafael:${DB_USER_PASS}@webpersonal-lzcy1.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
         if (err) {
             throw err;
